5b40f3d8135140def2645692240c7178ceeac42b
n1ck-kk <nick@leuer.com>
2020-10-14 12:27:59 -0400

    nick - Adding functioning backend. INCLUDING OLD USE functions for template and potential reuse

---
+++ backend/src/main/java/server/main.java
@@ -0,0 +1,48 @@
+package server;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class main {
+
+    @RequestMapping("/")
+	@CrossOrigin(origins = "http://localhost:3000") 	//test
+	//@CrossOrigin //(origins = "https://fiveo-clocksomewhere.firebaseapp.com/", maxAge =  3600, allowedHeaders = "*")     //production
+    String home() {
+		/*
+		try{
+			String url = "jdbc:mysql://localhost:3306/";	//test
+			url = "ofcmikjy9x4lroa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"; 	//deployment
+			//Connection conn = DriverManager.getConnection(url, "root", "1234DrinksWithFriends"); //test
+			Connection conn = DriverManager.getConnection(url, "iggb2ocyzjwrmrhp", "j9ta66pmslvbuok0"); //deployment
+			Statement smt = conn.createStatement();
+			String all="UserName\n\nPassword<br>";
+			
+			ResultSet rs = smt.executeQuery("select * from test_schema.user");
+			
+			while (rs.next())
+			{
+				all+=rs.getString("userName");
+				all+="\n\n";
+				all+=rs.getString("password");
+				all+="<br>";
+				System.out.println(all);
+			}
+			
+			conn.close();
+			return all;
+		}catch(Exception e){
+			e.printStackTrace();
+			return "FAIL";
+		}
+		*/
+		return "test";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(main.class, args);
+    }
+
+}
