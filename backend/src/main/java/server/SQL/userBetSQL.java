bed25cbae756ecf96278e3dfc6e0c707e9962258
n1ck-kk <nick@leuer.com>
2020-12-06 15:59:14 -0500

    nick - Added transaction on insertbets

--- backend/src/main/java/server/SQL/userBetSQL.java
+++ backend/src/main/java/server/SQL/userBetSQL.java
@@ -4,6 +4,8 @@ import server.userBets.userBet;
 import java.sql.*;
 import java.util.ArrayList;
 import server.Team.TeamStats;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class userBetSQL {
     private String url;
@@ -28,19 +30,63 @@ public class userBetSQL {
 
     public String insertBet(userBet betInfo) {
         try{
-			String query = "insert into "+ this.database+".userBets "+ 
-				"(userId, betId, amount, payout, betSuccess) "+
-				"values "+ 
-				"(?, ?, ?, ?, ?)";
-			psmt = conn.prepareStatement(query);
-			psmt.setLong(1, betInfo.getUserId());
-			psmt.setLong(2, betInfo.getBetId());
-			psmt.setLong(3, betInfo.getAmount());
-            psmt.setDouble(4, betInfo.getPayout());
-            psmt.setString(5, betInfo.getBetSuccess());
-            System.out.println(query);
-            psmt.executeUpdate();
-			psmt.close();
+			//Doing Transaction
+			// need to check if bet exists in betStats table first
+			conn.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
+			conn.setAutoCommit(false);
+
+			//check if bet still exists in betstats and that endDate is not past
+			String check = "select betId, endDate from "+this.database+".betStats where betId = ?;";
+			PreparedStatement psmtCheck = conn.prepareStatement(check);
+			psmtCheck.setLong(1, betInfo.getBetId());
+			rs = psmtCheck.executeQuery();
+
+			int existingBetId = 0;//rs.getInt("betId");
+			String endDate = "";//rs.getString("endDate");
+			if( rs.next() ) {
+				existingBetId = rs.getInt("betId");
+				endDate = rs.getString("endDate");
+			}
+			
+			System.out.println("endDate: "+endDate+" and betId: "+existingBetId);
+
+			Date currDate = new Date();
+			SimpleDateFormat fmt = new SimpleDateFormat("MM/dd/yy");
+			String now = fmt.format(currDate);
+			System.out.println("CurrDate: "+now);
+
+			Date db = new SimpleDateFormat("MM/dd/yyyy").parse(endDate);
+			Date insert = new SimpleDateFormat("MM/dd/yyyy").parse(now);
+			if ( !db.before(insert) && existingBetId == betInfo.getBetId() ) {
+				System.out.println("Date and betId are ok");
+				
+				//if ok, insert userbets
+				String query = "insert into "+ this.database+".userBets "+ 
+					"(userId, betId, amount, payout, betSuccess) "+
+					"values "+ 
+					"(?, ?, ?, ?, ?)";
+				psmt = conn.prepareStatement(query);
+				psmt.setLong(1, betInfo.getUserId());
+				psmt.setLong(2, betInfo.getBetId());
+				psmt.setLong(3, betInfo.getAmount());
+				psmt.setDouble(4, betInfo.getPayout());
+				psmt.setString(5, betInfo.getBetSuccess());
+				System.out.println(query);
+				psmt.executeUpdate();
+				
+				conn.commit();
+				psmt.close();
+			} else {
+				//otherwise rollback
+				conn.rollback();
+				psmtCheck.close();
+				conn.close();
+				return "ERROR INSERTING USERBET. BLOCKED BY TRANSACTION";
+			}
+			
+
+			psmtCheck.close();
+			//psmt.close();
             conn.close();
 			return "OK";
 		}catch(Exception e){
