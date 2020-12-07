f5a66a7ca1fe6453719b05c1121c66325262d224
Paul Miller <mill2136@purdue.edu>
2020-12-06 23:12:40 -0500

    betStats working but not placing a bet

--- frontend/src/components/BetAmount.js
+++ frontend/src/components/BetAmount.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Grid, Button, Modal, Card, CardContent, Header} from 'semantic-ui-react';
+import {Grid, Button, Modal, Card, CardContent, Header, Form} from 'semantic-ui-react';
 import '../css/CustomPopup.css'; 
 import '../css/SignIn.css';
 
@@ -8,114 +8,121 @@ export default class BetAmount extends React.Component{
 
     constructor(props){
         super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.togglePopup = this.togglePopup.bind(this);
         this.state = {
             betInfo: '',
             renderPopup: false,
             amount: 0
         }
+        this.handleClick = this.handleClick.bind(this);
+        this.togglePopup = this.togglePopup.bind(this);
     }
-
-    /* Create new userBet in database */
-   async togglePopup(e) {
-        e.preventDefault();
-        var id;
-        //GET USER ID
-        await fetch('http://localhost:8080/user/getIdByUsername', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: this.props.userInfo
-            })
-        }).then(res => res.json()).then((data) => {
-            console.log(data);
-            id = data;
-        }).catch(console.log)
-
-
-
-        //PARSE BET INFO FOR ID, CALCULATE PAYOUT BASED ON ODDS
-        var index = this.state.betInfo.indexOf("\"");
-        var betID = this.state.betInfo.substr(index + 1, this.state.betInfo.indexOf("\"", index + 1) - index - 1);
-        console.log(betID);
-        index = this.state.betInfo.indexOf("Odds");
-        var multiplier = this.state.betInfo.substr(index + 7, this.state.betInfo.indexOf(",", index + 1) - index -7 );
-        console.log(multiplier);
-        var payout = parseFloat(multiplier) * this.state.amount;
-        console.log(payout);
-        //console.log(this.props.userInfo);
-        await fetch('http://localhost:8080/userBets/insertUserBet', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                userId: id,
-                betId: betID,
-                amount: this.state.amount,
-                payout: payout,
-                betSuccess: "Pending"
-            })
-        }).then(res => res.text()).then((data) => {
-            console.log(data);
-            this.setState({  
-                betInfo: '',
-                renderPopup: false,
-                amount: 0
-            });
-        }).catch(console.log)
-    }
-
-    /* Render amount popup */
-    async handleClick (e) {
-        e.preventDefault();
-        this.setState({
-            betInfo: this.props.cardInfo,
-            renderPopup: true,
-            amount: 0
-        })
-    }
-
-        
-    handleChange =(e)=>{
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+    // BetId: 7
+    // EndDate: "03/24/21"
+    // Odds: 0.53
+    // StartDate: "03/23/19"
+    // TeamID: 20
+    // TeamName: "New York Knicks"
+    // Type: "Money Line"
 
     render() {
+        console.log(this.props.cardInfo);
         console.log(this.props.cardInfo['Type']);
         return(
            <div>
-               <Button onClick = {this.handleClick} top-margin = "0px">
-                    <Card style={{width: "500px"}} >
-                        <CardContent>
-                            <div className="left aligned">
-                                <Header> {this.props.cardInfo['Type']} </Header>
-                            </div>
-                        </CardContent>
-                    </Card>
+               <Button style={{width: '40vw'}} onClick = {this.handleClick} top-margin = "0px">
+                    <Header> {this.props.cardInfo['TeamName']}: {this.props.cardInfo['Type']} </Header>
+                    Odds: {this.props.cardInfo['Odds']}<br/>
+                    StartDate:{this.props.cardInfo['StartDate']} <br/>
+                    EndDate: {this.props.cardInfo['EndDate']}
+
+
                </Button>
                {this.state.renderPopup ? 
                 <div className='popup'>  
                     <div className='popup\_inner'>  
-                        <form onSubmit={this.togglePopup}>
+                        <Form onSubmit={this.togglePopup}>
                             <div className="input-field col s12">
                                 <input type="number" id="amount" placeholder="Bet Amount" onChange = {this.handleChange} />
                             </div>
                             <div className= "spacer"></div>
                             <button className = "btn btn-primary" type="submit">Make Bet</button>
-                        </form>
+                        </Form>
                     </div>  
                 </div>  
                 : ''}
             </div>
         )//End return(...)
     } //End render(){...}
+
+    /* Create new userBet in database */
+   async togglePopup(e) {
+    e.preventDefault();
+    var id;
+    //GET USER ID
+    await fetch('http://localhost:8080/user/getIdByUsername', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: this.props.userInfo
+        })
+    }).then(res => res.json()).then((data) => {
+        console.log(data);
+        id = data;
+    }).catch(console.log);
+
+
+
+    //PARSE BET INFO FOR ID, CALCULATE PAYOUT BASED ON ODDS
+    var index = this.state.betInfo.indexOf("\"");
+    var betID = this.state.betInfo.substr(index + 1, this.state.betInfo.indexOf("\"", index + 1) - index - 1);
+    console.log(betID);
+    index = this.state.betInfo.indexOf("Odds");
+    var multiplier = this.state.betInfo.substr(index + 7, this.state.betInfo.indexOf(",", index + 1) - index -7 );
+    console.log(multiplier);
+    var payout = parseFloat(multiplier) * this.state.amount;
+    console.log(payout);
+    //console.log(this.props.userInfo);
+    await fetch('http://localhost:8080/userBets/insertUserBet', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userId: id,
+            betId: betID,
+            amount: this.state.amount,
+            payout: payout,
+            betSuccess: "Pending"
+        })
+    }).then(res => res.text()).then((data) => {
+        console.log(data);
+        this.setState({  
+            betInfo: '',
+            renderPopup: false,
+            amount: 0
+        });
+    }).catch(console.log)
+}
+
+/* Render amount popup */
+async handleClick (e) {
+    e.preventDefault();
+    this.setState({
+        betInfo: this.props.cardInfo,
+        renderPopup: true,
+        amount: 0
+    })
+}
+
+    
+handleChange =(e)=>{
+    this.setState({
+        [e.target.id]: e.target.value
+    })
+}
 
 };
